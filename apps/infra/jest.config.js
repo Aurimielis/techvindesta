5683bc9a7a5ae1a2f04870fea4d5e39dfcc2bc76
Juozas Beniusis <jb@bluetel.co.uk>
2023-02-22 19:44:31 +0000

    Create sample app for cdk

---
+++ apps/infra/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/test'],
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  }
+};
