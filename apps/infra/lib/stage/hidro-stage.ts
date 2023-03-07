45fabd80598a3bbf9ff778b6c163ac62a2e68ead
Juozas Beniusis <jb@bluetel.co.uk>
2023-03-07 17:28:01 +0000

    Comment out vpc stack so it doesnt get created

--- apps/infra/lib/stage/hidro-stage.ts
+++ apps/infra/lib/stage/hidro-stage.ts
@@ -1,6 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { HidroVpcStack } from "../stack/hidro/hidro-vpc-stack";
+// import { HidroVpcStack } from "../stack/hidro/hidro-vpc-stack";
 
 /**
  * This is Hidro Stage
@@ -10,8 +10,8 @@ export class HidroStage extends cdk.Stage {
   constructor(scope: Construct, id: string, props: cdk.StageProps) {
     super(scope, id, props);
 
-    new HidroVpcStack(this, 'HidroVpcStack', {
-      vpcCidr: '172.18.0.0/16',
-    })
+    // new HidroVpcStack(this, 'HidroVpcStack', {
+    //   vpcCidr: '172.18.0.0/16',
+    // })
   }
 }
