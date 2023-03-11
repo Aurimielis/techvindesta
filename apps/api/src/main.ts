2a3f1d124890b69f2d2c460e450f8e93476d7331
Juozas Beniusis <jb@bluetel.co.uk>
2023-03-11 11:06:04 +0000

    Fix POST route and start working on ec2"

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,12 +1,23 @@
 import express from 'express';
+import bodyParser from "body-parser";
+import morgan from 'morgan';
 
 const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const port = process.env.PORT ? Number(process.env.PORT) : 3001;
 
 const app = express();
 
+app.use(morgan('tiny'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
+});
+
+app.post('/Post-dataHE.php', (req, res) => {
+  console.log("Does it get that far?")
+  res.send({ message: 'Hello, this should accept sensor data'});
 });
 
 app.listen(port, host, () => {
