314624d86780aa9d85962a558d91cf4f7c6e095f
Juozas Beniusis <jb@bluetel.co.uk>
2023-03-12 15:47:07 +0000

    Add step to delete processes before starting new one

--- scripts/start_express.sh
+++ scripts/start_express.sh
@@ -5,6 +5,9 @@ cd /home/ec2-user/app || exit
 # Set environment variables
 export NODE_ENV=production
 
-# Build app and start with pm2
+# Build app
 npm run api:build
-HOST=0.0.0.0 PORT=80 pm2 start dist/apps/api/main.js -f
+
+# Delete all pm2 processes and start a new one
+pm2 delete all
+HOST=0.0.0.0 PORT=80 pm2 start dist/apps/api/main.js
