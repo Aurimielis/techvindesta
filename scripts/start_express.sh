9c8f6717c0fa714d5efdb6484df7f8d5f6baf7d8
Juozas Beniusis <jb@bluetel.co.uk>
2023-03-12 14:48:00 +0000

    Try running as ec2-user

--- scripts/start_express.sh
+++ scripts/start_express.sh
@@ -4,9 +4,7 @@ cd /home/ec2-user/app || exit
 
 # Set environment variables
 export NODE_ENV=production
-export PORT=80
-export HOST=0.0.0.0
 
 # Build app and start with pm2
 npm run api:build
-pm2 start dist/apps/api/main.js -f
+su ec2-user -c "HOST=0.0.0.0 PORT=80 pm2 start dist/apps/api/main.js -f"
