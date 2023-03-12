b39bf6f19aff71fb7ed37c2eb0709bc6dcdd1d2d
Juozas Beniusis <jb@bluetel.co.uk>
2023-03-12 14:08:17 +0000

    See if that will install dev deps

--- scripts/install_express_deps.sh
+++ scripts/install_express_deps.sh
@@ -2,11 +2,6 @@
 . /home/ec2-user/.bashrc
 cd /home/ec2-user/app || exit
 
-# Set environment variables
-export NODE_ENV=production
-export PORT=80
-export HOST=0.0.0.0
-
 # Install node
 nvm install
 nvm use
@@ -14,3 +9,8 @@ nvm use
 # Install npm deps and pm2
 npm install
 npm -g install pm2
+
+# Set environment variables
+export NODE_ENV=production
+export PORT=80
+export HOST=0.0.0.0
