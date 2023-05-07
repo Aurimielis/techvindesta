65621169a21d7c1229824d96966be10933bf6772
Juozas Beniusis <jb@bluetel.co.uk>
2023-05-07 21:08:23 +0100

    Add back button

--- apps/web/pages/hidro.tsx
+++ apps/web/pages/hidro.tsx
@@ -1,13 +1,30 @@
 import Layout from '../src/components/layout/layout'
 import Header from '../src/components/header/header'
 import HidroDataTable from "../src/components/hidro-data-table/hidro-data-table";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 
 const endpoint = "https://lambda.techvindesta.com/he-data/"
+
+const BackBtn = styled.a`
+  cursor: pointer;
+  width: fit-content;
+  position: absolute;
+  top: 100px;
+
+  svg {
+    height: 1.4em;
+  }
+`
 
 const Hidro = ({ data, table }) => {
   return (
     <Layout>
       <Header header={"Hidro Data 📈"} postheader={table} />
+      <BackBtn href='/'>
+        <FontAwesomeIcon icon={faChevronLeft} />
+      </BackBtn>
       <HidroDataTable data={data} />
     </Layout>
   )
