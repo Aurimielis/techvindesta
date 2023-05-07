746038037d902965d31ce74900bb327ff9e2e932
Juozas Beniusis <jb@bluetel.co.uk>
2023-05-07 13:48:58 +0100

    This better fucking work

--- apps/web/pages/hidro.tsx
+++ apps/web/pages/hidro.tsx
@@ -4,14 +4,21 @@ import HidroDataTable from "../src/components/hidro-data-table/hidro-data-table"
 
 const endpoint = "https://lambda.techvindesta.com/he-data/"
 
-// const Hidro = ({ data, table }) => {
-const Hidro = () => {
+const Hidro = ({ data, table }) => {
   return (
     <Layout>
-      {/*<Header header={"Hidro Data 📈"} postheader={table} />*/}
-      {/*<HidroDataTable data={data} />*/}
+      <Header header={"Hidro Data 📈"} postheader={table} />
+      <HidroDataTable data={data} />
     </Layout>
   )
+}
+
+Hidro.getInitialProps = async (ctx) => {
+  const table = ctx.query['table-name'] ?? 'TestHE'
+
+  const res = await fetch(`${endpoint}${table}`)
+  const data = await res.json()
+  return { data, table }
 }
 
 export default Hidro
