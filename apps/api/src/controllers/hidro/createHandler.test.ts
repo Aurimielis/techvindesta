797c374a6bd07907f912882aa7bf296483b3cbfa
Juozas Beniusis <jb@bluetel.co.uk>
2023-05-24 21:14:15 +0100

    Update tests to reflect recent changes

--- apps/api/src/controllers/hidro/createHandler.test.ts
+++ apps/api/src/controllers/hidro/createHandler.test.ts
@@ -12,35 +12,40 @@ describe('POST /hidro/create', () => {
   it('should error if API key does not match', () => {
     return request(app)
       .post('/hidro/create')
-      .send({ apiKey: 'x' })
+      .type('form')
+      .send({ api_key: 'x' })
       .expect(409)
   })
 
   it('should fail for invalid sensor data', () => {
     return request(app)
       .post('/hidro/create')
-      .send({ apiKey: 'v', value1: "nope", value2: "2", value3: 3 })
+      .type('form')
+      .send({ api_key: 'v', value1: "nope", value2: "2", value3: 3 })
       .expect(400)
   })
 
   it('should fail for invalid HENr valid sensor data', () => {
     return request(app)
       .post('/hidro/create')
-      .send({ apiKey: 'v', value1: 1, value2: 2, value3: 3, HENr: "potato" })
+      .type('form')
+      .send({ api_key: 'v', value1: 1, value2: 2, value3: 3, HENr: "potato" })
       .expect(400)
   })
 
   it('should fail for non-existent table (HENr)', () => {
     return request(app)
       .post('/hidro/create')
-      .send({ apiKey: 'v', value1: 1, value2: 2, value3: 3, HENr: -1 })
+      .type('form')
+      .send({ api_key: 'v', value1: 1, value2: 2, value3: 3, HENr: -1 })
       .expect(400)
   })
 
   it('should accept and store valid sensor data', () => {
     return request(app)
       .post('/hidro/create')
-      .send({ apiKey: 'v', value1: "21.1", value2: 42, value3: 0.0, HENr: 27 })
+      .type('form')
+      .send({ api_key: 'v', value1: "21.1", value2: 42, value3: 0.0, HENr: 27 })
       .expect(200)
   })
 })
